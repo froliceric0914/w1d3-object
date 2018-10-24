a93241e4a43acdc2f37daf374ec4b1be67c06e5a
froliceric0914 <wzhao.eric@gmail.com>
2018-10-24 18:41:14 +0000

    letterMap

---
+++ letterMapping.js
@@ -0,0 +1,20 @@
+var str = process.argv[2];
+
+function countLetter(str){
+  var str = str.split(" ").join("").toLowerCase();
+  var counter = {};
+   for (var letter of str) {
+    counter[letter] = [];
+    var j = 0;
+  }
+  for (var letter of str) {
+    counter[letter].push(j);
+    j++;
+  }
+  return counter;
+}
+
+console.log(countLetter(str));
+
+
+
